b7d9ed30cbeb6749cac1cc0fcc80f78af63d7d1e
Maria Catharina van Veen <maria.vanVeen@Libertyserver.com>
2020-09-19 21:45:54 -0400

    JC 59

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+
+//VARIABLES
+var str1 = "This is the first sting. ", str2 = "This is the second string. ", str1 = str1.fontcolor("blue");
+window.alert(6 / 2);
+
+document.write(str1 + str2);
+
+//This function sets text and color of the #greenText
+function greenFunction() {
+  var str = "Now it is GREEN";
+  var result = str.fontcolor("green");
+  document.getElementById("greenText").innerHTML = result;
+}
