6e24041e694bce94eba4e3762ca081bc40a2edf7
Maria Catharina van Veen <maria.vanVeen@Libertyserver.com>
2020-09-22 18:11:44 -0400

    JC 156 slideshow with error
    Not wrapping around properly

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,13 +1,29 @@
-var slideIndex = 1;
-
+var slideIndex = 0;
 showSlides(slideIndex);
 
+//NEXT PREVIOUS CONTROLS
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//DOT CONTROLS
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
-  console.log(typeof slides);
-  console.log(slides);
-  console.log(typeof slides[0]);
-  console.log(slideIndex);
-  slides[slideIndex-1].style.display = "block";
+  var dots = document.getElementsByClassName("dot");
+  if (n >= slides.length) {slideIndex = 0;} // wrap around on the right
+  if (n < 0) {slideIndex = slides.length - 1;} // wrap around on the left
+  for (var i = 0; i < slides.length; i++) { // reset display to none for all mySlides
+    slides[i].style.display = "none";
+  }
+  for (var i = 0; i < dots.length; i++) { // reset dots to inactive for all dots
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+  slideIndex++;
+  setTimeout(showSlides, 2000);
 }
